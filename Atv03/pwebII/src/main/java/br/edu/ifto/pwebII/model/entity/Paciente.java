3c69591f499ee1c8c821158da2a13754256a8a17
heloisa45xp <heloisa.carvalho@estudante.ifto.edu.br>
2026-09-17 08:33:13 -0300

    Adaptar Paciente para herdar de PessoaFisica

--- Atv03/pwebII/src/main/java/br/edu/ifto/pwebII/model/entity/Paciente.java
+++ Atv03/pwebII/src/main/java/br/edu/ifto/pwebII/model/entity/Paciente.java
@@ -1,53 +1,35 @@
 package br.edu.ifto.pwebII.model.entity;
 
+import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-public class Paciente {
-    @Id
-    private long id;
-    private String nome;
-    private String telefone;
-
-    public Paciente(long id, String nome, String telefone) {
-        this.id = id;
-        this.nome = nome;
-        this.telefone = telefone;
-    }
+@DiscriminatorValue("PACIENTE")
+public class Paciente extends PessoaFisica {
 
     @OneToMany(mappedBy = "paciente")
     private List<Consulta> consultas = new ArrayList<>();
 
-    public long getId() {
-        return id;
+    public Paciente() {
     }
 
-    public void setId(long id) {
-        this.id = id;
+    public Paciente(String nome, String cpf, String email, String telefone) {
+        super(nome, cpf, email, telefone);
     }
 
-    public String getNome() {
-        return nome;
+    public List<Consulta> getConsultas() {
+        return consultas;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getTelefone() {
-        return telefone;
-    }
-
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
+    public void setConsultas(List<Consulta> consultas) {
+        this.consultas = consultas;
     }
 
     public String dados() {
-        return "Paciente: " + nome + " - Tel: " + telefone;
+        return "Paciente: " + getNome() + " - Tel: " + getTelefone();
     }
 }
